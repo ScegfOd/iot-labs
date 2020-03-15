76420b218bb383c49bffda2cc59da2a2462dd544
Jonathan Combs <JofCombs@gmail.com>
2020-03-15 14:59:08 -0700

    add lab 4 and half of 5

---
+++ lab4gps/include/Muh_LoRa.h
@@ -0,0 +1,12 @@
+#ifndef MUH_LORA
+#define MUH_LORA
+
+#define FREQUENCY 915E6 //915 MHz
+#define LORA_MISO 19
+#define LORA_MOSI 27
+#define LORA_CS   18
+#define LORA_SCK   5
+#define LORA_IRQ  26
+#define LORA_RST  14
+
+#endif
